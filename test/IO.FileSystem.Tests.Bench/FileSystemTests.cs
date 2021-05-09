bddaeb164a1c8c14e9151f669af5b4f9758bb194
waldosax <waldosax+github@gmail.com>
2021-05-09 15:25:33 -0400

    Something peculiar happpening with mapped drives not appearing.

---
+++ test/IO.FileSystem.Tests.Bench/FileSystemTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MayoSolutions.Framework.IO.Tests.Bench
+{
+    public class FileSystemTests
+    {
+        [Test]
+        public void EnsureMappedDrivesAreIncluded()
+        {
+            var fs = new FileSystem();
+            var drives = fs.Drive.GetDrives();
+        }
+    }
+}
